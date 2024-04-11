421ea78ccb7156944197924f90cf85bb4519db4e
Anjie Liu <30424333+anjliu@users.noreply.github.com>
2024-04-11 22:55:52 +0000

    mage init

--- 0_setup/init3_mage.sh
+++ 0_setup/init3_mage.sh
@@ -1,12 +1,8 @@
 mkdir ./1_mage
 cd 1_mage
 
-# git clone https://github.com/mage-ai/mage-ai-terraform-templates.git
-docker pull hashicorp/terraform:latest
+git clone https://github.com/mage-ai/compose-quickstart.git mage-quickstart \
+&& cd mage-quickstart \
+&& cp dev.env .env && rm dev.env \
+&& docker compose up
 
-# install Helm
-curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
-sudo apt-get install apt-transport-https --yes
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt-get update
-sudo apt-get install helm
